1bee4f15f569ea82a1d965d8fd029ec9e7b55c77
Yusin <sunkatabeckama@gmail.com>
2024-07-22 16:47:41 +0200

    Main file + router placement

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './main.scss';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Components
+import { store } from './redux/store';
+import Home from './pages/home/home';
+import Topbar from './components/navigation/topbar';
+import Footer from './components/footer/footer';
+import Profile from './pages/profil/profil';
+import Login from './pages/login/login';
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <>
+        <Topbar />
+        <Outlet />
+        <Footer />
+      </>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '*',
+        element: <div>Error</div>,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    element: (
+      <>
+        <Topbar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
