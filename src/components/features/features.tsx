268b5819aa33a7eadc03de9bb4a608a6fcce1c8b
Yusin <sunkatabeckama@gmail.com>
2024-07-22 16:48:11 +0200

    Features component

---
+++ src/components/features/features.tsx
@@ -0,0 +1,48 @@
+import styles from './features.module.scss';
+
+const Features = () => {
+  return (
+    <section className={styles['features']}>
+      <h2 className='sr-only'>Features</h2>
+      <div className={styles['feature-item']}>
+        <img
+          src='./assets/icon-chat.png'
+          alt='Chat Icon'
+          className={styles['feature-icon']}
+        />
+        <h3 className={styles['feature-item-title']}>
+          You are our #1 priority
+        </h3>
+        <p>
+          Need to talk to a representative? You can get in touch through our
+          24/7 chat or through a phone call in less than 5 minutes.
+        </p>
+      </div>
+      <div className={styles['feature-item']}>
+        <img
+          src='./assets/icon-money.png'
+          alt='Chat Icon'
+          className={styles['feature-icon']}
+        />
+        <h3 className={styles['feature-item-title']}>
+          More savings means higher rates
+        </h3>
+        <p>The more you save with us, the higher your interest rate will be!</p>
+      </div>
+      <div className={styles['feature-item']}>
+        <img
+          src='./assets/icon-security.png'
+          alt='Chat Icon'
+          className={styles['feature-icon']}
+        />
+        <h3 className={styles['feature-item-title']}>Security you can trust</h3>
+        <p>
+          We use top of the line encryption to make sure your data and money is
+          always safe.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
